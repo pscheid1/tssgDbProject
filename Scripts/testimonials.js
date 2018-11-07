420c6c1e99f81c1be382c40ab7fddbe37727b245
Jon Woolf <jwoolf@datacollaborative.com>
2018-11-07 17:15:36 -0500

    Initial commit for multipage design

---
+++ Scripts/testimonials.js
@@ -0,0 +1,46 @@
+// Javascript file that only contains the text of the testimonials.  Configured as an array of objects. 
+
+var testimonials = [
+     {   
+	name: "Alpha Primus",
+	text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut hendrerit justo dui, quis dapibus tortor lobortis et. Suspendisse rhoncus nulla sit amet bibendum efficitur. Cras rhoncus porttitor libero vel faucibus. Aenean consequat pretium fermentum. Donec et est ac tortor elementum lacinia id non magna. Phasellus ut odio in orci imperdiet pellentesque ac a quam. Vestibulum vel ligula finibus odio viverra suscipit non in magna. Phasellus eget vestibulum nisl. Aenean nisl justo, rutrum ut turpis vel, iaculis cursus turpis. Etiam non facilisis sem. Curabitur sit amet sapien ut purus auctor placerat nec sed nunc. Etiam blandit libero eget leo varius mollis. Nam ultricies dui at est ultricies, pharetra interdum ex lobortis. Sed tempor auctor fringilla. ",
+	date: "05/01/2017"
+     },
+     {   
+	name: "Beta Secundus",
+	text: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean finibus hendrerit neque, eu ornare tortor tristique vel. Donec dapibus tempus leo, vitae laoreet lectus vulputate sit amet. Fusce ultrices lorem a augue porttitor, at tincidunt eros viverra. Etiam eu tellus vel metus aliquet consequat vitae quis erat. Integer posuere, diam a varius feugiat, velit lorem auctor lorem, quis mollis turpis tellus non augue. Phasellus suscipit purus a nisl scelerisque auctor. Suspendisse cursus pellentesque vulputate. Nunc eu enim in nisi sagittis dapibus ac pulvinar lorem. Ut ornare vestibulum molestie. ",
+	date: "08/14/2017"
+     },
+     {   
+	name: "Gamma Tertius",
+	text: "Morbi mattis metus et tortor blandit elementum. Praesent faucibus tincidunt odio non molestie. Vestibulum quis pulvinar ipsum. Phasellus ac metus cursus, ultricies neque non, mollis est. Donec tristique consectetur purus, sit amet facilisis mauris elementum nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam est elit, fringilla in posuere nec, mollis vel felis. Vivamus quis dolor mollis, tincidunt lorem ac, mollis justo. Sed suscipit pretium odio a iaculis. Nam fermentum, nunc vel fringilla lacinia, nisl mi congue nulla, dictum vestibulum quam nulla quis velit. Suspendisse convallis tellus et dapibus vehicula. ",
+	date: "01/05/2018"
+     },
+     {   
+	name: "Delta Quartus",
+	text: "Nullam felis tellus, suscipit id odio a, gravida laoreet massa. Integer iaculis est vel efficitur elementum. Donec purus eros, tempus eget finibus non, condimentum id erat. Vestibulum ut turpis vel sapien tincidunt euismod nec ac elit. Aenean sed dui viverra, faucibus orci non, euismod enim. Nam dignissim et risus eget elementum. Curabitur dapibus tincidunt blandit. Nam sed ex sit amet justo dictum accumsan. Phasellus in augue tincidunt, congue lorem quis, gravida erat. Nam massa est, euismod id mauris convallis, commodo hendrerit libero. Sed dictum felis id nulla placerat accumsan. Morbi laoreet lacinia viverra. ",
+	date: "06/16/2018"
+     }
+]; // end array of testimonials
+
+var dataJSON = '[ {"name": "Alpha Primus","text": "alpha-text", "date": "05/01/2017" }]';
+
+
+
+		/*
+     {  "name": "Beta Secundus",
+		"text": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean finibus hendrerit neque, eu ornare tortor tristique vel. Donec dapibus tempus leo, vitae laoreet lectus vulputate sit amet. Fusce ultrices lorem a augue porttitor, at tincidunt eros viverra. Etiam eu tellus vel metus aliquet consequat vitae quis erat. Integer posuere, diam a varius feugiat, velit lorem auctor lorem, quis mollis turpis tellus non augue. Phasellus suscipit purus a nisl scelerisque auctor. Suspendisse cursus pellentesque vulputate. Nunc eu enim in nisi sagittis dapibus ac pulvinar lorem. Ut ornare vestibulum molestie. ",
+		"date": "08/14/2017"
+     },
+     {  "name": "Gamma Tertius",
+		"text": "Morbi mattis metus et tortor blandit elementum. Praesent faucibus tincidunt odio non molestie. Vestibulum quis pulvinar ipsum. Phasellus ac metus cursus, ultricies neque non, mollis est. Donec tristique consectetur purus, sit amet facilisis mauris elementum nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam est elit, fringilla in posuere nec, mollis vel felis. Vivamus quis dolor mollis, tincidunt lorem ac, mollis justo. Sed suscipit pretium odio a iaculis. Nam fermentum, nunc vel fringilla lacinia, nisl mi congue nulla, dictum vestibulum quam nulla quis velit. Suspendisse convallis tellus et dapibus vehicula. ",
+		"date": "01/05/2018"
+     },
+     {  "name": "Delta Quartus",
+		"text": "Nullam felis tellus, suscipit id odio a, gravida laoreet massa. Integer iaculis est vel efficitur elementum. Donec purus eros, tempus eget finibus non, condimentum id erat. Vestibulum ut turpis vel sapien tincidunt euismod nec ac elit. Aenean sed dui viverra, faucibus orci non, euismod enim. Nam dignissim et risus eget elementum. Curabitur dapibus tincidunt blandit. Nam sed ex sit amet justo dictum accumsan. Phasellus in augue tincidunt, congue lorem quis, gravida erat. Nam massa est, euismod id mauris convallis, commodo hendrerit libero. Sed dictum felis id nulla placerat accumsan. Morbi laoreet lacinia viverra. ",
+		"date": "06/16/2018"
+     }
+*/
+
+
+
--- readme.txt
+++ readme.txt
@@ -1,3 +1,2 @@
-This is the Minimum Viable Product of the new TSSG web site,
-implemented as a multi-page application.
-It's based on Bootstrap with a single scrolling page and a navbar at the top.
+This is the Minimum Viable Product of the new TSSG web site.
+It's a multipage design.
