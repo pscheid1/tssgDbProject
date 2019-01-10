4d27feff3d33dfe0571547976ad66b641dc7521e
joel sharasheff <jsharasheff@hotmail.com>
2019-01-10 10:49:12 -0500

    changed schedule

--- site/Scripts/scheduleDates.js
+++ site/Scripts/scheduleDates.js
@@ -30,15 +30,6 @@ var scheduleArray = [
     {
       "name": "item1",
       "month": "JAN",
-      "day": 09,
-      "year": 2019,
-      "startTime": "12:30",
-      "endTime": "5PM",
-      "location": acton
-    },
-    {
-      "name": "item2",
-      "month": "JAN",
       "day": 16,
       "year": 2019,
       "startTime": "12:30",
@@ -46,13 +37,22 @@ var scheduleArray = [
       "location": boxborough
     },
     {
-      "name": "item3",
+      "name": "item2",
       "month": "JAN",
       "day": 23,
       "year": 2019,
       "startTime": "12:30",
       "endTime": "5PM",
       "location": chelmsford
+    },
+    {
+      "name": "item3",
+      "month": "JAN",
+      "day": 30,
+      "year": 2019,
+      "startTime": "12:30",
+      "endTime": "5PM",
+      "location": acton
     }
 ];
  
