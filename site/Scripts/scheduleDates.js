3e39363e498a5a83f7db9b512ebf91fac2d0d624
joel sharasheff <jsharasheff@hotmail.com>
2019-06-26 20:59:59 -0400

    changed schedule

--- site/Scripts/scheduleDates.js
+++ site/Scripts/scheduleDates.js
@@ -29,15 +29,6 @@ var boxborough = new Location("Boxborough","Images/icon-lib-boxborough.png","htt
 var scheduleArray = [ 
       {
         "name": "item1",
-        "month": "JUNE",
-        "day": 26,
-        "year": 2019,
-        "startTime": "12:30",
-        "endTime": "5PM",
-        "location": chelmsford
-      },
-      {
-        "name": "item2",
         "month": "JULY",
         "day": 3,
         "year": 2019,
@@ -46,9 +37,18 @@ var scheduleArray = [
         "location": acton
       },
       {
-        "name": "item3",
+        "name": "item2",
         "month": "JULY",
         "day": 10,
+        "year": 2019,
+        "startTime": "12:30",
+        "endTime": "5PM",
+        "location": acton
+      },
+      {
+        "name": "item3",
+        "month": "JULY",
+        "day": 17,
         "year": 2019,
         "startTime": "12:30",
         "endTime": "5PM",
