7709f8124b275d93ac69dd2eac8bc3ce9bd1248a
Paul Scheidemantel <paul.scheidemantel@gmail.com>
2019-08-18 14:29:00 -0400

    Change Del btn text from black to white

--- src/app/user-get/user-get.component.ts
+++ src/app/user-get/user-get.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { User } from 'src/app/_models/user';
 import { UserService } from 'src/app/_services/user.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Role } from '../_models/role';
+import { ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
 
 @Component({
@@ -38,21 +36,4 @@ export class UserGetComponent implements OnInit {
       this.userFromApi = user;
     });
   }
-/*
-  deleteUser(_id) {
-    this.us.deleteUser(_id)
-      .then(res => {
-        this.ngOnInit(); // refresh the page after deletion.
-      })
-      .catch(err => {
-        this.errorMsg = err;
-        // ensure href does not already contain '#bottom'
-        // if not, add '#bottom' to scroll page to bottom to
-        // insure error message is visable
-        if ((window.location.href).indexOf('#bottom') < 0) {
-          window.location.href = window.location.href + '#bottom';
-        }
-      })
-  }
- */
 }
