fba7b71200b7e07072b10b60eae4ff0e4bd59b1a
Paul Scheidemantel <paul.scheidemantel@gmail.com>
2019-08-22 17:35:41 -0400

    Added team-get components

--- src/app/team-get/team-get.component.ts
+++ src/app/team-get/team-get.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Team } from 'src/app/_models/Team';
+import { TeamService } from '../_services/team.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-team-get',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team-get.component.css']
 })
 export class TeamGetComponent implements OnInit {
-
-  constructor() { }
+teams: Team[];
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private ts: TeamService) { }
 
   ngOnInit() {
+    this.ts.getteams().subscribe((data: Team[]) => {
+      this.teams = data;
+    });
   }
 
+  deleteTeam(_id) {
+    this.ts.deleteteam(_id).subscribe(res => {
+      this.ngOnInit(); // added by ps to refresh the page after deletion.
+    });
+  }
 }
