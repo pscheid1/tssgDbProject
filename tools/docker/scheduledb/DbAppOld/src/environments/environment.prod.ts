b709f2a9dcf447461610e28d8db97836618497b3
Ralph A. Navarro Jr <ralph@navarrocomputing.com>
2019-12-01 22:20:44 -0500

    Remove Paul's old DbAppOld and DbApiOld

--- tools/docker/scheduledb/DbAppOld/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  TSSGAPPURL: $ENV.TSSGAPPURL,
-  TSSGAPP_PORT: $ENV.TSSGAPP_PORT,
-  TSSGAPIURL: $ENV.TSSGAPIURL,
-  TSSGAPIPORT: $ENV.TSSGAPIPORT
-};
