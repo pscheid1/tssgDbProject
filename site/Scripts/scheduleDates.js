d083c0ec327e648e018a24b24bd96d70ab68726a
joel sharasheff <jsharasheff@hotmail.com>
2020-01-18 11:52:14 -0500

    changed schedule

--- site/Scripts/scheduleDates.js
+++ site/Scripts/scheduleDates.js
@@ -31,15 +31,6 @@ var scheduleArray = [
     {
       "name": "item1",
       "month": "JANUARY",
-      "day": 15,
-      "year": 2020,
-      "startTime": "12:30",
-      "endTime": "5PM",
-      "location": acton
-    },
-    {
-      "name": "item2",
-      "month": "JANUARY",
       "day": 22,
       "year": 2020,
       "startTime": "12:30",
@@ -47,9 +38,18 @@ var scheduleArray = [
       "location": chelmsford
     },
     {
-      "name": "item3",
+      "name": "item2",
       "month": "JANUARY",
       "day": 29,
+      "year": 2020,
+      "startTime": "12:30",
+      "endTime": "5PM",
+      "location": acton
+    },
+    {
+      "name": "item3",
+      "month": "FEBRUARY",
+      "day": 5,
       "year": 2020,
       "startTime": "12:30",
       "endTime": "5PM",
