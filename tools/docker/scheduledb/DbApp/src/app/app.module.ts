cc165179294fcf4da731dd79f430b3a536fc9aa6
Paul Scheidemantel <paul.scheidemantel@gmail.com>
2020-03-19 13:20:02 -0400

    Added Authentication, Team and User services to providers.  Added APP_BASE_HREF to providers.

--- tools/docker/scheduledb/DbApp/src/app/app.module.ts
+++ tools/docker/scheduledb/DbApp/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { AuthenticationService } from './_services/authentication.service';
+import { MeetingService } from './_services/meeting.service';
+import { TeamService } from './_services/team.service';
+import { UserService } from './_services/user.service';
+import { VenueService } from './_services/venue.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeetingAddComponent } from './meeting-add/meeting-add.component';
 import { MeetingEditComponent } from './meeting-edit/meeting-edit.component';
 import { MeetingGetComponent } from './meeting-get/meeting-get.component';
-import { MeetingService } from './_services/meeting.service';
 import { VenueAddComponent } from './venue-add/venue-add.component';
 import { VenueGetComponent } from './venue-get/venue-get.component';
 import { VenueEditComponent } from './venue-edit/venue-edit.component';
-import { VenueService } from './_services/venue.service';
 import { FormsModule } from '@angular/forms';
 import { JwtInterceptor } from './_helpers/jwt.interceptor';
 import { ErrorInterceptor } from './_helpers/error.interceptor';
@@ -34,49 +38,55 @@ import { TeamEditComponent } from './team-edit/team-edit.component';
 import { TeamGetComponent } from './team-get/team-get.component';
 import { MeetingGetScheduleComponent } from './meeting-get-schedule/meeting-get-schedule.component';
 
+import { APP_BASE_HREF } from '@angular/common';
+
 @NgModule({
-   declarations: [
-      AppComponent,
-      MeetingAddComponent,
-      MeetingEditComponent,
-      MeetingGetComponent,
-      VenueAddComponent,
-      VenueGetComponent,
-      VenueEditComponent,
-      HomeComponent,
-      UserGetAllComponent,
-      LoginComponent,
-      LogoutComponent,
-      UserCreateComponent,
-      UserGetCurrComponent,
-      UserEditComponent,
-      NavbarComponent,
-      TeamAddComponent,
-      TeamEditComponent,
-      TeamGetComponent,
-      MeetingGetScheduleComponent
+  declarations: [
+    AppComponent,
+    MeetingAddComponent,
+    MeetingEditComponent,
+    MeetingGetComponent,
+    VenueAddComponent,
+    VenueGetComponent,
+    VenueEditComponent,
+    HomeComponent,
+    UserGetAllComponent,
+    LoginComponent,
+    LogoutComponent,
+    UserCreateComponent,
+    UserGetCurrComponent,
+    UserEditComponent,
+    NavbarComponent,
+    TeamAddComponent,
+    TeamEditComponent,
+    TeamGetComponent,
+    MeetingGetScheduleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    TimepickerModule.forRoot(),
+    BrowserAnimationsModule
+  ],
+  providers: [
+  AuthenticationService,
+  MeetingService,
+  TeamService,
+  UserService,
+  VenueService,
+  { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  { provide: ErrorHandler, useClass: TssgErrorHandler },
+  { provide: APP_BASE_HREF, useValue: '/' }
    ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      ReactiveFormsModule,
-      HttpClientModule,
-      FormsModule,
-      BsDropdownModule.forRoot(),
-      BsDatepickerModule.forRoot(),
-      TimepickerModule.forRoot(),
-      BrowserAnimationsModule
-   ],
-   providers: [
-      MeetingService,
-      VenueService,
-      { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-      { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-      { provide: ErrorHandler, useClass: TssgErrorHandler }
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  bootstrap: [
+  AppComponent
+]
 })
 
-export class AppModule {}
+export class AppModule { }
