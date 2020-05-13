f16d869b181b01b409cc0a87f91d27b81ab0bee5
Ralph A. Navarro Jr <ralph@navarrocomputing.com>
2020-05-13 12:05:06 -0400

    TWS-311 Add test case for "Verify menu appears when hamburger is clicked on narrow and short page" TWS-299 Refactor with screen size variables and use setViewportSize().  * TSSGTechBasePage.groovy - Added isWebElementVisible(), setViewportSize()  * TSSGTechSpec.groovy    * Added new test case for TWS-311.    * Refactored TWS-299.

--- tests/GebAndSpock/src/test/groovy/Common/TSSGTechBasePage.groovy
+++ tests/GebAndSpock/src/test/groovy/Common/TSSGTechBasePage.groovy
@@ -1,12 +1,15 @@
 package Common
 
 import geb.Page
+import org.openqa.selenium.Dimension
+import org.openqa.selenium.Point
+import org.openqa.selenium.WebElement
 
 class TSSGTechBasePage extends Page {
 
     static at = {
         //title == "Technical Skills Share Group (TSSG)"
-        $("strong", text:"GENERAL MEETING INFORMATION")
+        $("strong", text:"GENERAL MEETING INFORMATION").displayed
     }
 
     static content = {
@@ -32,4 +35,43 @@ class TSSGTechBasePage extends Page {
         devopsFooterIcon { $("#devopsGroup").parent() }
 
     }
+
+    /**
+     * Checks if the web element is displayed within the viewport.
+     *
+     * @param WebElement w
+     */
+    boolean isWebElementVisible(WebElement w) {
+        Dimension weD = w.size
+        Point weP = w.location
+
+        int x = browser.js."window.innerWidth"
+        int y = browser.js."window.innerHeight"
+        int x2 = weD.width + weP.x
+        int y2 = (weD.height * 0.6) + weP.y
+        println "isWebElementVisible " +
+            "( weD.x:w.innerWidth, weD.y:w.innerHeight ) : " +
+            "( ${x2}:${x}, ${y2}:${y} )"
+        println "  window.innerHeight = ${y}"
+        println "  weD.height         = ${weD.height}"
+        println "  weP.y              = ${weP.y}"
+        println "  y2                 = ${y2}"
+
+        return x2 <= x && y2 <= y
+    }
+
+    /**
+     * Set the viewport size.
+     *
+     * @param int width Width of the viewport (i.e. browser's viewable width)
+     * @param int hwight Height of the viewport (i.e. browser's viewable height)
+     */
+    def setViewportSize(int width, int height) {
+        println "Setting viewport size = ${width}w, ${height}h"
+        driver.manage().window().size = new Dimension(
+            browser.js."window.outerWidth - window.innerWidth + ${width}" as int,
+            browser.js."window.outerHeight - window.innerHeight + ${height}" as int
+        )
+    }
+
 }
--- tests/GebAndSpock/src/test/groovy/Tests/TSSGTechSpec.groovy
+++ tests/GebAndSpock/src/test/groovy/Tests/TSSGTechSpec.groovy
@@ -9,7 +9,6 @@ import Pages.TSSGTechSchedulePage
 import Pages.TSSGTechWebPage
 import Pages.TSSGTechQAPage
 import geb.spock.GebReportingSpec
-import spock.lang.Ignore
 import spock.lang.Unroll
 import spock.lang.IgnoreRest
 import org.openqa.selenium.Dimension
@@ -83,16 +82,41 @@ class TSSGTechSpec extends GebReportingSpec {
     }
 
     //@IgnoreRest
-    def "Verify hamburger menu appears in header on narrow page"() {
+    def "Verify hamburger icon appears in header on narrow page"() {
         testKey = "TWS-299"
 
         given: "Set up a narrow browser window"
-            driver.manage().window().size = new Dimension(640, 960)
+            to TSSGTechHomePage
+            int width = 640
+            int height = 960
+            setViewportSize(width, height)
 
         when: "Navigate to the Tssg web page"
             to TSSGTechHomePage
 
         then: "the hamburger icon is displayed"
             hamburgerButton
+    }
+
+    //@IgnoreRest
+    def "Verify menu appears when hamburger is clicked on narrow and short page"() {
+        testKey = "TWS-311"
+
+        given: "Set up a narrow and short browser window"
+            to TSSGTechHomePage
+            int width = 640
+            int height = 320
+            setViewportSize(width, height)
+
+        when: "user navigates to any tssg.tech web page"
+            to TSSGTechHomePage
+        then: "hamburger icon is displayed"
+            hamburgerButton
+
+        when: "hamburger icon is clicked"
+            hamburgerButton.click()
+        then: "hamburger menu is expanded"
+        and: "bottom most menu item is accessible"
+            isWebElementVisible(devopsMenu.firstElement())
     }
 }
