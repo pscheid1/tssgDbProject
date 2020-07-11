8676db81d3eea95c6b47588903eea0cb7c1f600b
Paul Scheidemantel <paul.scheidemantel@gmail.com>
2020-07-10 23:47:26 -0400

    Updated environment variables

--- tools/docker/scheduledb/DbApp/custom-webpack-config.js
+++ tools/docker/scheduledb/DbApp/custom-webpack-config.js
@@ -4,12 +4,10 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       $ENV: {
-        TSSGAPPURL: JSON.stringify(process.env.tssgAppURL || 'http://localhost'),
-        // TSSGAPP_PORT: JSON.stringify(process.env.tssgAppPort || '4300'),
-        TSSGAPP_PORT: JSON.stringify(process.env.tssgAppPort || '4200'),
-        TSSGAPIURL: JSON.stringify(process.env.tssgApiURL || 'http://localhost'),
-        TSSGAPIPORT: JSON.stringify(process.env.tssgApiPort || '7010')
-        // TSSGAPIPORT: JSON.stringify(process.env.tssgApiPort || '4433')
+        TSSGAPPURL: JSON.stringify(process.env.FRONTEND_URL || 'http://localhost'),
+        TSSGAPP_PORT: JSON.stringify(process.env.FRONTEND_URL || '4200'),
+        TSSGAPIURL: JSON.stringify(process.env.BACKEND_BASE_URL || 'http://localhost'),
+        TSSGAPIPORT: JSON.stringify(process.env.BACKEND_BASE_PORT || '7010')
       }
     })
   ]
