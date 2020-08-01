4d0867f620a6ad72b01dcf0d946f2a865b3359c3
Paul Scheidemantel <paul.scheidemantel@gmail.com>
2020-08-01 14:03:51 -0400

    Forced user/register to role admin corrected spelling error

--- tools/docker/scheduledb/DbApi/routes/user.route.js
+++ tools/docker/scheduledb/DbApi/routes/user.route.js
@@ -10,7 +10,7 @@ const user_controller = require('../controllers/user.controller');
 userRouter
 // authenticate and register are public routes
 .post('/authenticate', user_controller.authenticate)
-.post('/register', user_controller.register)
+.post('/register', authorize([Role.Admin]), user_controller.register)
 .post('/update', authorize([Role.Admin, Role.User]), user_controller.update)
 // findAll limited to roll Admin
 .get('/', authorize([Role.Admin]), user_controller.findAll)
@@ -18,7 +18,7 @@ userRouter
 .get('/list', authorize([Role.Admin]), user_controller.listUsers)
 // contacts returns all users with role = Contact
 .get('/contacts', authorize([Role.Admin]), user_controller.listContacts)
-// getCurrent retrieves the user imbeded in the token regardless of role
+// getCurrent retrieves the user embeded in the token regardless of role
 .get('/current', authorize([]), user_controller.getCurrent)
 // getById - Read/Update for user's account
 //         - All operations for Admin on all accounts
