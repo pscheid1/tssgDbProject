58c5287d2530f2fe64c49f53c78804ca86c65c2f
Paul Scheidemantel <paul.scheidemantel@gmail.com>
2020-09-21 13:13:22 -0400

    Delete old/unused code.

--- tools/docker/scheduledb/DbApp/src/environments/environment.prod.ts
+++ tools/docker/scheduledb/DbApp/src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
 
-  // with the implementation of set-env.ts, .env, and env.js, the
-  // following code is nolonger needed.
-
-  // TSSGAPPURL: $ENV.TSSGAPPURL,
-  // TSSGAPP_PORT: $ENV.TSSGAPP_PORT,
-  // TSSGAPIURL: $ENV.TSSGAPIURL,
-  // TSSGAPIPORT: $ENV.TSSGAPIPORT
 };
