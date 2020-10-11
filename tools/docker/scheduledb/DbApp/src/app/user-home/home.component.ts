43956ee28b4691da0fc25d536a453b33865de404
Paul Scheidemantel <paul.scheidemantel@gmail.com>
2020-10-11 17:47:38 -0400

    backendVersion changed to BACKEND_VERSION frontendVersion changed to FRONTEND_VERSION Code added to detect expired JWT at home page load

--- tools/docker/scheduledb/DbApp/src/app/user-home/home.component.ts
+++ tools/docker/scheduledb/DbApp/src/app/user-home/home.component.ts
@@ -4,6 +4,10 @@ import { UserService } from 'src/app/_services/user.service';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EnvService } from '../env.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+const jwtHelper = new JwtHelperService();
+
 @Component({
   selector: 'app-user-home',
   templateUrl: './home.component.html',
@@ -16,19 +20,19 @@ export class HomeComponent implements OnInit {
   dspCreatedDate = '';
   errorMsg = '';
   ngVersion = '10';  // initialize to default value
-  backendVersion;
-  frontendVersion;
+  BACKEND_VERSION;
+  FRONTEND_VERSION;
 
   constructor(
     private route: ActivatedRoute,
     private us: UserService,
     private router: Router,
-    private authenticationService: AuthenticationService,
+    private as: AuthenticationService,
     private env: EnvService
   ) {
-    this.currentUser = this.authenticationService.currentUserValue;
-    this.backendVersion = env.BACKEND_VERSION;
-    this.frontendVersion = env.FRONTEND_VERSION;
+    this.currentUser = this.as.currentUserValue;
+    this.BACKEND_VERSION = env.BACKEND_VERSION;
+    this.FRONTEND_VERSION = env.FRONTEND_VERSION;
   }
 
   ngOnInit() {
@@ -42,7 +46,29 @@ export class HomeComponent implements OnInit {
       // console.log('home.component.ts.currentUser is null ');
       this.router.navigate(['user/login']);
     }
-    // console.error('home.component.ts.currentUser: ' + this.currentUser);
+
+    // console.error('home.component.ts.currentUser: ' + this.currentUser.username);
+
+    // cannot use as.currentUserValue as it is cached. Get currentUser directly from local storage
+    const user: User = JSON.parse(localStorage.getItem('currentUser'));
+    // console.log(`auth.guard.canActivate current user: ${JSON.stringify(user)}`);
+    const token = this.currentUser.token;
+    // console.log(`auth.guard.canActivate current access token: ${token}`);
+
+    // const userTokenInfo = this.jwtService.decodeToken(token);
+    // const cuTokenInfo = this.jwtService.decodeToken(cu.token);
+    // const currentTime = new Date().getTime() / 1000;
+    // console.log(`\nauth.guard.canActivate currentTime: ${currentTime}`);
+    // console.log(`auth.guard.canActivate user.token expiring time: ${userTokenInfo.exp}`);
+    // console.log(`auth.guard.canActivate cu.token expiring time: ${cuTokenInfo.exp}\n`)
+
+    // Check whether token is valid.  If expired, display msg & go to user/login page; otherwise continue
+    if (jwtHelper.isTokenExpired(token)) {
+      // access jwt expired, force a logout
+      this.as.logout();
+      alert('M007\nYour access JWT has expired.\nYou have been automatically logged out.\nPlease login again.');
+      this.router.navigate(['user/login']);
+    }
 
     const body = document.getElementsByTagName('body');
     if (body.length > 0) {
